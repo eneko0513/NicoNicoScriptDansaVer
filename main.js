b337183f21652b9fd5d0e540c7a92a91c7ecb858
eneko0513 <eneko0513@users.noreply.github.com>
2018-02-25 00:25:46 +0900

    v
    v

--- main.js
+++ main.js
@@ -303,7 +303,7 @@ javascript: (function(f, dd) {
         '</div>' +
         */
         '</div>' +
-        '<div style = "clear:both;"><p class="VideoUploadDateMeta-title"><font color="red">バージョン：' + VersionInfo + '</font></p><p>更新概要：' + VersionDescript + '</p></div>' +
+        '<p class="VideoUploadDateMeta-title"><font color="red">バージョン：' + VersionInfo + '</font></p><p>更新概要：' + VersionDescript + '</p></div>' +
         '';
 
         e.after(f);
